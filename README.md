d28a90cfedceae1e1352d87470dbcac9c0411c62
Raymond-Suarez <raymondsuarez1407@gmail.com>
2021-10-26 21:24:56 -0400

    5 files modified

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # react-dbchgn
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-dbchgn)
+Esta es la tarea #4 de Raymond Suarez, esta es la captura de pantalla:
+
+![Captura de Pantalla]()
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./style.css";
+import React, {Component} from 'react';
+import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends Component 
+{
+  render()
+  {
+    return(
+      <center>
+      <div class="Form">
+        <h1>Agenda React</h1>
+        <form>
+          <div class="inp1 font">
+          <label>Nombre</label>
+          <input type="" name="nombre"/>
+          </div>
+          <div class="inp2 font">
+          <label>Apellido</label>
+          <input type="" name="apellido"/>
+          </div>
+          <div class="inp3 font">
+          <label>Telefono</label>
+          <input type="" name="Telefono"/>
+          </div>
+        </form> 
+      </div>
+      </center>
+    )
+  }
 }
+export default App;
